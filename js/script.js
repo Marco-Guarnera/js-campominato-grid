1a6a0ed509da129c2c578ccbec4e76feedfe3408
Marco-Guarnera <Darkaquon93@gmail.com>
2024-08-26 08:28:13 +0200

    Added Click Function to Cells

--- js/script.js
+++ js/script.js
@@ -31,10 +31,13 @@ buttonElement.addEventListener("click", function () {
         const cell = createNode("div", "cell");
         // Aggiungo il numero della cella
         cell.append(i + 1);
+        // Aggiungo un EventListener alla cella
+        cell.addEventListener("click", function () {
+            cell.classList.toggle("clicked");
+            // Stampo il numero della cella in console
+            console.log(cell.innerText);
+        });
         // Aggiungo la cella nel DOM
         gridElement.appendChild(cell);
     }
 });
-
-// Fase di raccolta dati
-// Fase di produzione
